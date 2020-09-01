c05b7f6f9bc999f86a30d33510d2ef9c0b4640ba
Will Shelley <38144750+williamshelley@users.noreply.github.com>
2020-09-01 17:50:15 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dev:kdwfHroz5L7IScY8@cluster0.nwabl.mongodb.net/realeasy?retryWrites=true&w=majority",
-    secretOrKey: 'PsE5WXmLP^B&BOs0a55,Qhlrm<p)C8',
-};
