3f9d9115e290fb8678105b3c7d3ca6134e66c6c4
William Shelley <wjshelley5018@gmail.com>
2020-10-31 05:13:33 -0400

    Removed commented out sections

--- frontend/src/util/api/project_api_util.js
+++ frontend/src/util/api/project_api_util.js
@@ -4,17 +4,9 @@ export const postNewProject = project => {
   return axios.post("api/projects/new", { project });
 }
 
-// export const getOneProject = id => {
-//   return axios.get(`api/projects/${id}`);
-// }
-
 export const getOneProject = id => {
   return axios.post("api/projects/get-one", { filters: { id } });
 }
-
-// export const getManyUserProjects = userId => {
-//   return axios.get(`api/projects/users/${userId}`);
-// }
 
 export const getManyUserProjects = user => {
   return axios.post("api/projects/get-many", { filters: { user, owner: user } })
